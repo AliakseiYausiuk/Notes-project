31206ab90fb446e72c8f54e84a61b30edc9139f1
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-04-13 21:24:04 +0300

    feat: add new layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
-import {Navbar} from './components/navbar/Navbar';
+import React from "react";
+import "./App.css";
+import { Navbar } from "./components/navbar/Navbar";
+import { Board } from "./components/board/Board";
+import { Box } from "@mui/material";
 
 function App() {
+  const styleNavabr = {
+    position: "absolute" as "absolute",
+    top: "1%",
+    left: "1%",
+  };
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Box
+      className="App"
+      sx={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "start",
+        alignItems: "center",
+        padding: "0 20%",
+      }}
+    >
+      <Box sx={styleNavabr}>
+        <Navbar />
+      </Box>
+      <Board />
+    </Box>
   );
 }
 
