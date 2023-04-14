6b4a1c4b2b03dbefbd9d1d4f644fb9ce596238aa
Aliaksei Yausiuk <Aliakseiyausiuk@gmail.com>
2023-04-14 17:59:20 +0300

    feat: add new layout

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,16 +1,42 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, ButtonGroup, IconButton, Typography } from "@mui/material";
 import { Card } from "../card/Card";
+import AddIcon from "@mui/icons-material/Add";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 
 export const Board = () => {
   return (
     <Box
       sx={{
-        width: "200px",
-        height: "500px",
-        backgroundColor: "lightgray",
+        width: "300px",
+        height: "190px",
+        backgroundColor: "#fafafa",
+        borderRadius: "5px",
       }}
     >
+      <Box display={"flex"}>
+        <Typography
+          variant={"h5"}
+          align={"center"}
+          sx={{
+            margin: "0 0 0 10px",
+          }}
+        >
+          Status
+        </Typography>
+        <ButtonGroup
+          sx={{
+            margin: "0 0 0 140px",
+          }}
+        >
+          <IconButton>
+            <AddIcon />
+          </IconButton>
+          <IconButton>
+            <MoreHorizIcon />
+          </IconButton>
+        </ButtonGroup>
+      </Box>
       <Card />
     </Box>
   );
